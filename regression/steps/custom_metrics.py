f7489c476ce153985fd9aec1ea5e6737eae2e28d
Sunish Sheth <sunishsheth2009@gmail.com>
2022-11-02 10:20:00 -0700

    Fix custom_metrics.py

--- regression/steps/custom_metrics.py
+++ regression/steps/custom_metrics.py
@@ -21,8 +21,8 @@ from sklearn.metrics import mean_squared_error
 
 def weighted_mean_squared_error(
     eval_df: DataFrame,
-    builtin_metrics: Dict[str, int],  # pylint: disable=unused-argument
-) -> int:
+    builtin_metrics: Dict[str, float],  # pylint: disable=unused-argument
+) -> float:
     """
     Computes the weighted mean squared error (MSE) metric.
 
