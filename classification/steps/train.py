dfc23d1744f3377408f2abd6a663a29c85818d3d
sunishsheth2009@gmail.com <sunishsheth2009@gmail.com>
2023-01-18 23:37:03 +0000

    Update to use xgboost

--- classification/steps/train.py
+++ classification/steps/train.py
@@ -13,8 +13,10 @@ def estimator_fn(estimator_params: Dict[str, Any] = None):
     The estimator's input and output signatures should be compatible with scikit-learn
     estimators.
     """
-    from sklearn.linear_model import SGDClassifier
+#     from sklearn.linear_model import SGDClassifier
 
-    if estimator_params is None:
-        estimator_params = {}
-    return SGDClassifier(random_state=42, **estimator_params)
+#     if estimator_params is None:
+#         estimator_params = {}
+#     return SGDClassifier(random_state=42, **estimator_params)
+    import xgboost as xgb
+    return xgb.XGBClassifier()
